a9b8d426921f05d8caef28f9a023c8016a6c6d45
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-09-22 18:38:55 -0400

    added variable and main functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.getElementById("#password");
+// character codes in ASCII to avoid listing each character 
+var UPPERCASE_CHAR_CODES = arrayFormLowToHigh(48, 57);
+var SYMBOLS_CHAR_CODES = arrayFormLowToHigh(33, 47).concat(arrayFormLowToHigh(58, 64).concat
+(arrayFormLowToHigh(91, 96)).concat(arrayFormLowToHigh(123, 126))
+)
+var LOWERCASE_CHAR_CODES = arrayFormLowToHigh(65, 90);
+var NUMBER_CHAR_CODES = arrayFormLowToHigh(97, 122);
+
+generateBtn.addEventListener("click", e=> {
+  e.preventDefault()
+  var password 
+  writePassword()
+  
+}) 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var characterAmountEL = prompt("How long do you want your password? (8-128 characters)")
+  var includeLowercaseEL = confirm("Click 'ok' to include lowercase letters")
+  var includeUppercaseEL = confirm("Click 'ok' to include uppercase letters")
+  var includeNumbersEL = confirm("Click 'ok' to include numbers")
+  var includeSymbolsEL = confirm("Click 'ok' to include symbols")
+  var includeLowercase = includeLowercaseEL.value
+  var includeUppercase = includeUppercaseEL.value
+  var includeNumbers = includeNumbersEL.value
+  var includeSymbols = includeSymbolsEL.value
+  var characterAmount = characterAmountEL.value
 
-  passwordText.value = password;
+  function generatePassword(characterAmount, includeLowercase, includeNumbers, includeSymbols, includeUppercase) {
+    var charCodes = []
+    if (includeLowercase) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES)
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES) 
+    
+    var passwordCharacters = []
+    for (var i = 0; i < characterAmount; i++) {
+        var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+        passwordCharacters.push(String.fromCharCode(characterCode))
+    }
+    return passwordCharacters.join('')
+  }
 
+
+  
+  // passwordText.value = password;
+  passwordText.innerHTML = passwordCharacters;
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// a loop to finds character sets for each type
+function arrayFormLowToHigh(low, high) {
+  var array = []
+  for (var i = low; i <= high; i++) {
+  array.push(i)
+  }
+  return array
+}
 
-console.log("hello world, I am here.")
